f865c111e6bff8b72213a088b068ab4f9748f91b
Vitali <secstudy2018@gmail.com>
2019-06-13 17:08:21 +0300

    camelCase

--- Lession19/StarEx4/main.js
+++ Lession19/StarEx4/main.js
@@ -1,11 +1,18 @@
-class Player{
-    constructor(name){
-        this.name = name;
-        this.score = 0;
-        
-    }
+class Player {
+  constructor(name) {
+    this.name = name;
+    this.score = 0;
+  }
 }
 
-    let player = new player(name)
-
-
+let player = new player(name);
+function play(player) {
+  let num = Math.floor(Math.random() * (10 - 1 + 1)) + 1;
+  var isEven = function(number) {
+    if (isEven % 2 === 0) {
+      console.log(`player win!`);
+    } else {
+        console.log(`player lose!`);
+    }
+  };
+}
